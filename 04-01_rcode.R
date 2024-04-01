2c2fc12b6c79256889fe0ec84a650662db9d24ad
lihsinchien <lihsinchien226@gmail.com>
2024-04-01 17:13:07 +0800

    04-01 Rcode

--- 04-01_rcode.R
+++ 04-01_rcode.R
@@ -1,14 +1,19 @@
+
 ##################################################
 ### Ch3 model diagnosis (03-25_rcode.R) ##########
 ##################################################
 
-data<-read.table(file="savings.txt",header=T)
+getwd() #get the working directory
+setwd("C:\\Users\\user\\Documents\\LHChien") #set the working directory
+#data<-read.table(file="C:\\Users\\user\\Documents\\LHChien\\savings.txt",header=T)
+data<-read.table(file="savings.txt",header=T) 
+
 head(data)
 dim(data)
-?savings
 
-lmod<- lm(sr~.,savings)
+lmod<- lm(sr~.,data=data)
 summary(lmod)
+
 lmod.f<-step(lmod)
 summary(lmod.f)
 
@@ -16,8 +21,8 @@ par(mfrow=c(2,2))
 plot(lmod.f) 
 
 dev.off()
-plot(lmod,which=5)
-plot(lmod,which=4)
+plot(lmod.f,which=5)
+plot(lmod.f,which=4)
 
 ##################################################
 ### Ch4 Classification Method
@@ -43,59 +48,3 @@ summary(glm.fit)
 
 contrasts(Direction) #check the coding: R code "Direction" as the dummy variable
 
-# prediction
-glm.probs <- predict(glm.fit , type = "response")
-glm.probs[1:10]
-
-glm.xb <- predict(glm.fit)
-tmp<-glm.xb[1:10]
-exp(tmp)/(1+exp(tmp))
-
-# model performance: make the Confusion Matrix
-glm.pred<- ifelse(glm.probs>.5,"Up","Down")
-table(glm.pred,Direction)
-
-(507+145)/1250 #correctly classified proportion
-mean(glm.pred == Direction)
-
-## the training error is often overly optimistic
-## split data into training set and testing set:
-## training set: "Year" < 2005
-
-table(Year)
-train <- (Year < 2005)
-head(train)
-table(train)
-head(!train)
-table(!train)
-
-Smarket.2005 <- Smarket[!train, ]
-dim(Smarket.2005)
-Direction.2005 <- Direction[!train]
-
-## fit the model using the training data
-glm.fit.train <- glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume ,
-  data = Smarket , family = binomial , subset = train)
-summary(glm.fit.train)
-
-# model performance using the testing set 
-glm.probs <- predict(glm.fit.train, Smarket.2005, type="response")
-glm.pred <- ifelse(glm.probs> .5,"Up","Down")
-table(glm.pred, Direction.2005)
-mean(glm.pred==Direction.2005) # worse than random selection
-mean(glm.pred!=Direction.2005)
-
-
-glm.fit.train2 <- glm(Direction ~ Lag1 + Lag2 , data = Smarket ,
-                family = binomial , subset = train)
-summary(glm.fit.train2)
-
-glm.probs <- predict(glm.fit.train2, Smarket.2005, type="response")
-glm.pred <- ifelse(glm.probs> .5,"Up","Down")
-table(glm.pred, Direction.2005)
-mean(glm.pred==Direction.2005)
-106/(106+76) # conditional on predict "Up", 58% accuracy rate
-
-x.new<-data.frame(Lag1 = c(1.2, 1.5), Lag2 = c(1.1, -0.8))
-x.new
-predict(glm.fits, newdata = x.new, type = "response")
